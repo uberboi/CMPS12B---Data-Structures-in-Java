9960e6a6d5d85bc1be79be09a8f0de70fd8cc794
sthuang <sthuang@ucsc.edu>
2018-09-19 15:02:11 -0700

    removed directory

--- lab7/KeyCollisionException.java
+++
@@ -1,7 +0,0 @@
-//-----------------------------------------------------------------------------
-// KeyCollisionException.java
-//-----------------------------------------------------------------------------
-
-public class KeyCollisionException extends RuntimeException{
-   public KeyCollisionException(String s){ super(s); }
-}
--- lab7/KeyNotFoundException.java
+++
@@ -1,7 +0,0 @@
-//-----------------------------------------------------------------------------
-// KeyNotFoundException.java
-//-----------------------------------------------------------------------------
-
-public class KeyNotFoundException extends RuntimeException{
-   public KeyNotFoundException(String s){ super(s);}
-}
